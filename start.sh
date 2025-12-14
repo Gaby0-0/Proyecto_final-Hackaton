72ce4545a2a63ce3dc0dc68db2354d64ba576d59
Gaby0-0 <gabriela0203martinez@gmail.com>
2025-12-14 00:24:33 -0600

    configuracion del seed

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 # Ejecutar migraciones
 php artisan migrate --force
-php artisan migrate --force
+php artisan db:seed --force 
 # Limpiar y cachear configuración
 php artisan config:clear
 php artisan config:cache
