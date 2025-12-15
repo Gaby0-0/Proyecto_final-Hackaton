89d7869f11c1cd3341792ecc84852825673bc6aa
Paola719 <paohernandez651@gmail.com>
2025-12-15 13:51:59 -0600

    Fix: Quitar cola de notificaciones para envío inmediato de informes por email

--- app/Notifications/InformeGeneralNotification.php
+++ app/Notifications/InformeGeneralNotification.php
@@ -4,11 +4,10 @@ namespace App\Notifications;
 
 use Barryvdh\DomPDF\Facade\Pdf;
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class InformeGeneralNotification extends Notification implements ShouldQueue
+class InformeGeneralNotification extends Notification
 {
     use Queueable;
 
